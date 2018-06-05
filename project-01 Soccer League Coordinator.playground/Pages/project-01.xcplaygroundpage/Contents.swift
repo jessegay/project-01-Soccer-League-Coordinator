03d339e7dd4f8e9accd8db477c3f577b0dc05c46
Jesse Gay <jesse@teamtreehouse.com>
2018-06-05 14:21:02 -0700

    Fixed duplicate team name
    Accidentally pasted teamDragons where I needed teamRaptors.

--- project-01 Soccer League Coordinator.playground/Pages/project-01.xcplaygroundpage/Contents.swift
+++ project-01 Soccer League Coordinator.playground/Pages/project-01.xcplaygroundpage/Contents.swift
@@ -58,7 +58,7 @@ var inexperiencedPlayers: [[String: Any]] = []
 //** Part 2: ** Create logic to iterate through all 18 players and assign them to teams such that the number of experienced players on each team are the same. Store each team’s players in its own collection for use in Part 3. Please note: your logic should work correctly regardless of the initial ordering of the players and the number of players. This solution should work if there are 18 players or 100.
 
 
-// Divide the players into two groups, one for the experienced   players, another one for the inexperienced players.
+// Divide the players into two groups, one for the experienced players, another one for the inexperienced players.
 
 for player in theLeague {
     if player["isExperienced"] as? Bool == true {
@@ -148,7 +148,7 @@ print("teamSharks' average height is \(heightTotalSharks/numberOfSharks) inches.
 
 
 var heightTotalRaptors = 0.0
-for player in teamDragons {
+for player in teamRaptors {
     if let height = player["height"] as? Double {
     heightTotalRaptors += height
     }
@@ -156,7 +156,7 @@ for player in teamDragons {
 let numberOfRaptors: Double = Double(teamRaptors.count)
 print("teamRaptors' average height is \(heightTotalRaptors/numberOfRaptors) inches.")
 
-
+print("\n")
 //    ** Part 3: ** Write code that iterates through all three teams of players and generates a personalized letter to the guardians, letting them know which team the child has been placed on and when they should attend their first team team practice. As long as you provide the necessary information (player name, team name, guardian names, practice date/time), feel free to have fun with the content of the letter. The team practice dates/times are as follows:
 //
 //Dragons - March 17, 1pm
